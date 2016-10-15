4725f097402a43d4e1188a0e0f0fce53fd99bf43
cangzhang <cangzan@gmail.com>
2016-10-15 12:06:59 +0800

    Update test.

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,54 @@
 <template>
-    <div id="app">
-        <img class="logo" src="./assets/logo.png">
-        <hello></hello>
-        <el-button type="primary" @click.native="openDialog">{{ btnTitle }}</el-button>
-        <div class="LoginDialog">
-            <el-dialog title="收货地址" v-model="dialogFormVisible">
-                <el-form :model="form">
-                    <el-form-item label="活动名称" :label-width="formLabelWidth">
-                        <el-input v-model="form.name" auto-complete="off"></el-input>
-                    </el-form-item>
-                    <el-form-item label="活动区域" :label-width="formLabelWidth">
-                        <el-select v-model="form.region" placeholder="请选择活动区域">
-                            <el-option label="区域一" value="shanghai"></el-option>
-                            <el-option label="区域二" value="beijing"></el-option>
-                        </el-select>
-                    </el-form-item>
-                </el-form>
-                <span slot="footer" class="dialog-footer">
-                  <el-button @click.native="dialogFormVisible = false">取 消</el-button>
-                  <el-button type="primary" @click.native="dialogFormVisible = false">确 定</el-button>
-                </span>
-            </el-dialog>
-        </div>
-    </div>
+  <div id="app">
+    <img class="logo" src="./assets/logo.png">
+    <hello></hello>
+    <login-dialog></login-dialog>
+
+  </div>
 </template>
 <script>
-import Hello from './components/Hello'
-// import LoginDialog from './components/LoginDialog'
 
-export default {
+  import Hello from './components/Hello'
+  import LoginDialog from './components/LoginDialog'
+
+  export default {
     components: {
-        Hello,
-        // LoginDialog
+      Hello,
+      LoginDialog
     },
     data: () => {
-        return {
-            form: {},
-            btnTitle: 'Open Dialog',
-            dialogFormVisible: false,
-            formLabelWidth: '200'
-        }
-    },
-    methods: {
-        openDialog: function() {
-            this.dialogFormVisible = true;
-        }
+      return {}
     }
-}
+  }
 </script>
-<style>
-html {
-    height: 100%;
-}
 
-body {
+<style>
+  html {
+    height: 100%;
+  }
+
+  body {
     display: flex;
     align-items: center;
     justify-content: center;
     height: 100%;
-}
+  }
 
-#app {
+  #app {
     color: #2c3e50;
     margin-top: -100px;
     max-width: 600px;
     font-family: Source Sans Pro, Helvetica, sans-serif;
     text-align: center;
-}
+  }
 
-#app a {
+  #app a {
     color: #42b983;
     text-decoration: none;
-}
+  }
 
-.logo {
+  .logo {
     width: 100px;
     height: 100px
-}
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import $ from "jquery"
+
+// import $ from "jquery"
 import Vue from 'vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
