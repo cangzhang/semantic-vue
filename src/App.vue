b4525d87bdfc052616718c147e4df672eed736db
cangzhang <cangzan@gmail.com>
2017-02-20 22:11:19 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,61 @@
 <template>
-<div id="app">
-  <!-- <img class="logo" src="./assets/logo.png"> -->
-  <hello></hello>
-  <login-modal :display="display" ref="semModal"></login-modal>
-  <button class="ui primary button" @click="openModal">
-    OPEN Modal
-  </button>
-</div>
+    <div id="app">
+        <img class="logo" src="./assets/logo.png">
+        <hello></hello>
+        <login-modal :display="display" ref="semModal"></login-modal>
+        <button class="ui primary button" @click="openModal">
+            OPEN Modal
+        </button>
+    </div>
 </template>
-
 <script>
 import Hello from './components/Hello'
 import LoginModal from './components/LoginModal'
 
 export default {
-  components: {
-    Hello,
-    LoginModal
-  },
-  computed: {
-    display() {
-      return this.$store.state.UserControl.loginModalStatus
+    components: {
+        Hello,
+        LoginModal
+    },
+    computed: {
+        display() {
+            return this.$store.state.UserControl.loginModalStatus
+        }
+    },
+    data() {
+        return {}
+    },
+    methods: {
+        openModal() {
+            this.$store.commit('switchModalStatus')
+        }
     }
-  },
-  data() {
-    return {}
-  },
-  methods: {
-    openModal() {
-      this.$store.commit('switchModalStatus')
-    }
-  }
 }
 </script>
-
-<style>
+<style lang="scss">
 html {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
+    height: 100%;
+    body {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        #app {
+            color: #2c3e50;
+            margin-top: -100px;
+            max-width: 600px;
+            font-family: Source Sans Pro, Helvetica, sans-serif;
+            text-align: center;
+            a {
+                color: #42b983;
+                text-decoration: none;
+            }
+        }
+    }
 }
 
 .logo {
-  width: 100px;
-  height: 100px
+    width: 100px;
+    height: 100px
 }
 </style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,23 +1,21 @@
 <template>
-<div class="hello">
-  <div class="aaa"></div>
-  <h2>{{ msg }}</h2>
-</div>
+    <div class="hello">
+        <div class="aaa"></div>
+        <h2>{{ msg }}</h2>
+    </div>
 </template>
-
 <script>
 export default {
-  data() {
-    return {
-      msg: 'Hello!'
+    data() {
+        return {
+            msg: 'Hello!'
+        }
     }
-  }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h2 {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
