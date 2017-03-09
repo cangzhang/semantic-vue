9d867f817398ff890fa4ac8bf5541af6581f219f
cangzhang <cangzan@gmail.com>
2017-03-09 22:58:03 +0800

    Update struc.

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import svButton from './buttons/button.vue'
+import svLabeledButton from './buttons/labeled-button.vue'
+import svAnimatedButton from './buttons/animated-button.vue'
+import svButtonGroup from './buttons/button-group.vue'
+import svDivider from './divider/divider.vue'
+import svInput from './input/input.vue'
+
+export default [
+  svButton,
+  svLabeledButton,
+  svAnimatedButton,
+  svButtonGroup,
+  svDivider,
+  svInput
+]
