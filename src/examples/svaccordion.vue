c56cae037c7c26cfe4ccc79a179f4bafb2e6d66f
cangzhang <cangzan@gmail.com>
2017-06-13 22:44:05 +0800

    update template

--- src/examples/svaccordion.vue
+++ src/examples/svaccordion.vue
@@ -26,6 +26,32 @@
         </span>
       </sv-accordion-item>
     </sv-accordion>
+        <sv-accordion v-model="actives2">
+      <sv-accordion-item name="1">
+        <span slot="title">What is a dog?</span>
+        <span slot="content">
+          A dog is a type of domesticated animal.
+        </span>
+      </sv-accordion-item>
+      <sv-accordion-item name="2">
+        <span slot="title">What kinds of dogs are there?</span>
+        <span slot="content">
+          There are many breeds of dogs. Each breed varies in size and temperament.
+          Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.
+        </span>
+      </sv-accordion-item>
+      <sv-accordion-item name="3">
+        <span slot="title">How do you acquire a dog?</span>
+        <span slot="content">
+          Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.
+          <br>
+          A pet shop may be the most convenient way to buy a dog.
+          <br>
+          Buying a dog from a private owner allows you to assess the pedigree and upbringing of your dog before choosing to take it home.
+          Lastly, finding your dog from a shelter, helps give a good home to a dog who may not find one so readily.
+        </span>
+      </sv-accordion-item>
+    </sv-accordion>
   </div>
 </template>
 
@@ -35,7 +61,8 @@
     props: {},
     data() {
       return {
-        actives: ['1', '2']
+        actives: ['1', '2'],
+        actives2: ['3']
       }
     }
   }
